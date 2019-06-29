a39b7a26bf3a0d349b1dfb5a9356cd7f4d913dd0
Tim Matthias Jentzsch <tim-jentzsch@gmx.de>
2019-06-29 18:06:23 +0200

    Add array contains test

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,15 +1,39 @@
-import { mapAsync, filterAsync } from './util';
+import { mapAsync, filterAsync, contains } from './util';
 
-test('mapAsync works', async () => {
-  const testArray = [1, 2, 3];
-  const asyncFunction = async (el: number) => el + 1;
-  const rr = await mapAsync(testArray, asyncFunction);
-  expect(rr).toEqual([2, 3, 4]);
-});
+describe('Util', () => {
+  describe('map async', () => {
+    test('works', async () => {
+      const testArray = [1, 2, 3];
+      const asyncFunction = async (el: number) => el + 1;
+      const rr = await mapAsync(testArray, asyncFunction);
+      expect(rr).toEqual([2, 3, 4]);
+    });
+  });
 
-test('filterAsync works', async () => {
-  const testArray = [1, 2, 3];
-  const asyncFunction = async (el: number) => el > 2;
-  const rr = await filterAsync(testArray, asyncFunction);
-  expect(rr).toEqual([3]);
+  describe('filter async', () => {
+    test('works', async () => {
+      const testArray = [1, 2, 3];
+      const asyncFunction = async (el: number) => el > 2;
+      const rr = await filterAsync(testArray, asyncFunction);
+      expect(rr).toEqual([3]);
+    });
+  });
+
+  describe('contains', () => {
+    test('true', () => {
+      const testArray = [1, 2, 3, 4];
+      const testNumber = 3;
+      const actual = contains(testArray, testNumber);
+
+      expect(actual).toEqual(true);
+    });
+
+    test('false', () => {
+      const testArray = [1, 2, 3, 4];
+      const testNumber = 5;
+      const actual = contains(testArray, testNumber);
+
+      expect(actual).toEqual(false);
+    });
+  });
 });
