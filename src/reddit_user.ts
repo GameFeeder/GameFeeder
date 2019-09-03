56cf161e6e32739b44d05024973bd65946b12ad2
zachkont <z.kontoulis@gmail.com>
2019-09-03 10:29:40 +0300

    Change all remaining filter to titleFilter and improve logging

--- src/reddit_user.ts
+++ src/reddit_user.ts
@@ -7,16 +7,15 @@ export default class RedditUserProvider {
 
   constructor(name: string, titleFilter?: string | RegExp) {
     this.name = name;
-    botLogger.debug(`Making filters for ${this.name}`, this.logLabel);
     if (typeof(titleFilter) === 'string' || titleFilter instanceof String) {
       this.titleFilter = new RegExp(titleFilter);
-      botLogger.debug(`Regex filter created from ${titleFilter}`, this.logLabel);
+      botLogger.debug(`Regex filter created from '${titleFilter}' for ${this.name}`, this.logLabel);
     } else if (titleFilter instanceof RegExp) {
       this.titleFilter = titleFilter;
-      botLogger.debug(`Filter ${titleFilter} is already a regex`, this.logLabel);
+      botLogger.debug(`Filter '${titleFilter}' is already a regex for ${this.name}`, this.logLabel);
     } else {
       this.titleFilter = /.*/;
-      botLogger.warn(`Filter ${titleFilter} cannot become a regex - \
+      botLogger.warn(`Filter '${titleFilter}' for ${this.name} cannot become a regex - \
         using /.*/ instead`, this.logLabel);
     }
   }
