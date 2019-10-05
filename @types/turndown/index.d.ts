8385a687a04655ed8edf0e9c4d8a15f58cf25e3e
Nikolaos Stamatopoulos <stamatopoulosnikos@gmail.com>
2019-10-05 16:19:31 +0200

    Prettify @types

--- @types/turndown/index.d.ts
+++ @types/turndown/index.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="node" />
 
 declare module 'turndown' {
-
   export default class TurndownService {
     /** Converts a HTML string to a Markdown string.
-     * 
+     *
      * @param htmlText - The HTML text to parse to Markdown.
      * @returns The converted Markdown string.
      */
