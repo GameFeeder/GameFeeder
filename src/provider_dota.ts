fe8d9885d9236503a8622f97697d8f5c5fdd8bf0
Nikolaos Stamatopoulos <stamatopoulosnikos@gmail.com>
2019-10-05 16:38:45 +0200

    Prettify a leftover in src

--- src/provider_dota.ts
+++ src/provider_dota.ts
@@ -80,7 +80,7 @@ export default class DotaProvider extends Provider {
     const patchList: string[] = [];
     const $ = pageDoc;
 
-    $('#PatchSelector option').each(function () {
+    $('#PatchSelector option').each(function() {
       const option = $(this).val();
       // botLogger.info(option);
       if (option !== 'Select an Update...') {
