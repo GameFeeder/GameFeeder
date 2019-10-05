1a947758913f451b0febb8858b1d30a3578a1407
Nikolaos Stamatopoulos <stamatopoulosnikos@gmail.com>
2019-10-05 16:19:31 +0200

    Prettify @types

--- @types/feed-read/feed-read.d.ts
+++ @types/feed-read/feed-read.d.ts
@@ -1,20 +1,32 @@
 /// <reference types="node" />
 
 declare module 'feed-read_wrapper' {
+  export type Feed = { name: string; source: string; link: string };
 
-  export type Feed = {name: string, source: string, link: string};
-
-  export type Article = {title: string, author: string, link: string, content: string, timestamp: Date, feed: Feed};
+  export type Article = {
+    title: string;
+    author: string;
+    link: string;
+    content: string;
+    timestamp: Date;
+    feed: Feed;
+  };
 
   export function parseURL(url: string, callback: (err: Error, articles: Article[]) => void): void;
 
-  export function parseRSS(rss_string: string, callback: (err: Error, articles: Article[]) => void): void;
+  export function parseRSS(
+    rss_string: string,
+    callback: (err: Error, articles: Article[]) => void,
+  ): void;
 
-  export function parseAtom(atom_string: string, callback: (err: Error, articles: Article[]) => void): void;
+  export function parseAtom(
+    atom_string: string,
+    callback: (err: Error, articles: Article[]) => void,
+  ): void;
 
-  export type FeedReader = { 
-    parseURL(url: string, callback: (err: Error, articles: Article[]) => void): void,
-    parseRSS(rss_string: string, callback: (err: Error, articles: Article[]) => void): void,
-    parseAtom(atom_string: string, callback: (err: Error, articles: Article[]) => void): void
+  export type FeedReader = {
+    parseURL(url: string, callback: (err: Error, articles: Article[]) => void): void;
+    parseRSS(rss_string: string, callback: (err: Error, articles: Article[]) => void): void;
+    parseAtom(atom_string: string, callback: (err: Error, articles: Article[]) => void): void;
   };
 }
