68fcad16e24fb543401d8411c07754fb186a1450
Tim Matthias Jentzsch <tim-jentzsch@gmx.de>
2019-10-08 15:47:09 +0200

    Refactor Reddit User logger to the new logging system

--- src/reddit_user.ts
+++ src/reddit_user.ts
@@ -1,24 +1,27 @@
-import botLogger from './bot_logger';
+import Logger from './bot_logger';
 
 export default class RedditUserProvider {
   public name: string;
   public titleFilter: RegExp;
-  private logLabel = 'reddit_user';
+  public static logger = new Logger('Reddit User');
 
   constructor(name: string, titleFilter?: string | RegExp) {
     this.name = name;
     if (typeof titleFilter === 'string' || titleFilter instanceof String) {
       this.titleFilter = new RegExp(titleFilter);
-      botLogger.debug(`Regex filter created from '${titleFilter}' for ${this.name}`, this.logLabel);
+      RedditUserProvider.logger.debug(
+        `Regex filter created from '${titleFilter}' for ${this.name}`,
+      );
     } else if (titleFilter instanceof RegExp) {
       this.titleFilter = titleFilter;
-      botLogger.debug(`Filter '${titleFilter}' is already a regex for ${this.name}`, this.logLabel);
+      RedditUserProvider.logger.debug(
+        `Filter '${titleFilter}' is already a regex for ${this.name}`,
+      );
     } else {
       this.titleFilter = /.*/;
-      botLogger.warn(
+      RedditUserProvider.logger.warn(
         `Filter '${titleFilter}' for ${this.name} cannot become a regex - \
         using /.*/ instead`,
-        this.logLabel,
       );
     }
   }
