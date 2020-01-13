b5a8d5bded68512f686511c6207dd277e25092e8
TimJentzsch <tim-jentzsch@gmx.de>
2020-01-13 15:19:13 +0100

    Refactor notifications (#167)
    * Add builder-like pattern to notification class to simplify object construction
    
    * Apply new notification construction pattern
    
    * Merge notification text and description to content attribute
    
    * Add notification function to apply the default game footer and game color
    
    * Remove duplicate dota provider file
    
    * Update project version to 0.7.2

--- src/reddit.ts
+++ src/reddit.ts
@@ -107,20 +107,16 @@ export default class Reddit {
 
         for (const post of posts) {
           // Convert the post into a notification
-          const notification = new Notification(
-            game,
-            `New post by ${user.name}!`,
-            new NotificationElement(post.title, post.url),
-            this.mdFromReddit(post.selftext),
-            new Date(post.created_utc * 1000),
-            null, // post.thumbnail,
-            null,
-            new NotificationElement(
+          const notification = new Notification(new Date(post.created_utc * 1000))
+            .withGameDefaults(game)
+            .withTitle(post.title, post.url)
+            .withAuthor(
               `/u/${user.name}`,
               `https://www.reddit.com/user/${user.name}`,
               'https://www.redditstatic.com/new-icon.png',
-            ),
-          );
+            )
+            .withContent(this.mdFromReddit(post.selftext));
+
           notifications.push(notification);
         }
       } catch (error) {
