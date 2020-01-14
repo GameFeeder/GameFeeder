c173b29cedc9ae417768d86232c33f8f0f927144
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-01-14 12:12:58 +0100

    Refactor submission validity checks in separate functions

--- src/reddit/reddit.ts
+++ src/reddit/reddit.ts
@@ -88,26 +88,9 @@ export default class Reddit {
       try {
         Reddit.logger.debug(`Getting posts from /u/${user.name} on /r/${subreddit}...`);
         const allPosts = await reddit.getUser(user.name).getSubmissions();
-        const posts = allPosts.filter((submission) => {
-          // Check if the post is new
-          const timestamp = new Date(submission.created_utc * 1000);
-          const isNew = timestamp > date;
-          // Check if the subreddit is correct
-          const isCorrectSub = submission.subreddit_name_prefixed === `r/${subreddit}`;
-          // Check if the url is already covered by other providers
-          let isNewSource = true;
-          for (const filter of urlFilters) {
-            const alreadyCovered = new RegExp(filter).test(submission.url);
-            if (alreadyCovered) {
-              isNewSource = false;
-            }
-          }
-          // Check the title to determine if the post is an update
-          const isValidTitle = user.titleFilter.test(submission.title);
-          // Check if the post has been deleted
-          const isDeleted = /^\[removed\]$/.test(submission.selftext);
-          return isNew && isCorrectSub && isValidTitle && isNewSource && !isDeleted;
-        });
+        const posts = allPosts.filter((submission) =>
+          this.isValidSubmission(submission, subreddit, user, date, urlFilters),
+        );
 
         for (const post of posts) {
           // Convert the post into a notification
@@ -131,6 +114,56 @@ export default class Reddit {
     notifications = sortLimitEnd(notifications, limit);
 
     return notifications;
+  }
+
+  /** Determines if the submission is valid. */
+  private static isValidSubmission(
+    submission: Snoowrap.Submission,
+    subreddit: string,
+    user: RedditUserProvider,
+    date: Date,
+    urlFilters: string[],
+  ) {
+    return (
+      this.isNew(submission, date) &&
+      this.isCorrectSub(submission, subreddit) &&
+      this.isValidTitle(submission, user) &&
+      this.isNewSource(submission, urlFilters) &&
+      !this.isDeleted(submission)
+    );
+  }
+
+  /** Checks if the url is already covered by other providers. */
+  private static isNewSource(submission: Snoowrap.Submission, urlFilters: string[]): boolean {
+    let isNewSource = true;
+    for (const filter of urlFilters) {
+      const alreadyCovered = new RegExp(filter).test(submission.url);
+      if (alreadyCovered) {
+        isNewSource = false;
+      }
+    }
+    return isNewSource;
+  }
+
+  /** Checks the title to determine if the post is an update. */
+  private static isValidTitle(submission: Snoowrap.Submission, user: RedditUserProvider): boolean {
+    return user.titleFilter.test(submission.title);
+  }
+
+  /** Checks if the submission is new. */
+  private static isNew(submission: Snoowrap.Submission, date: Date): boolean {
+    const timestamp = new Date(submission.created_utc * 1000);
+    return timestamp > date;
+  }
+
+  /** Checks if the subreddit is correct. */
+  private static isCorrectSub(submission: Snoowrap.Submission, subreddit: string): boolean {
+    return submission.subreddit_name_prefixed === `r/${subreddit}`;
+  }
+
+  /** Checks if the post has been deleted. */
+  private static isDeleted(submission: Snoowrap.Submission): boolean {
+    return /^\[removed\]$/.test(submission.selftext);
   }
 
   private static mdFromReddit(text: string): string {
