f877e2136ae01ffd1575d237a272f7da3aff55ae
Tim Jentzsch <tim-jentzsch@gmx.de>
2020-02-07 10:58:18 +0100

    Add command group

---
+++ src/commands/command_group.ts
@@ -0,0 +1,51 @@
+import Command from './command';
+import BotClient from '../bots/bot';
+import Channel from '../channel';
+import { UserRole } from '../user';
+import Message from '../message';
+
+export default class CommandGroup extends Command {
+  public regexStr: (bot: BotClient, channel: Channel) => Promise<string>;
+  public defaultAction: (message: Message, match: RegExpMatchArray) => Promise<void>;
+  public commands: Command[];
+
+  constructor(
+    name: string,
+    description: string,
+    channelLabel: (channel: Channel) => Promise<string>,
+    channelTrigger: (channel: Channel) => Promise<RegExp>,
+    defaultAction: (message: Message, match: RegExpMatchArray) => Promise<void>,
+    commands: Command[],
+    role?: UserRole,
+  ) {
+    super(
+      name,
+      description,
+      channelLabel,
+      channelTrigger,
+      async (message, match) => {
+        const { group } = match.groups;
+
+        // Execute the first matching sub-command
+        for (const cmd of this.commands) {
+          const regex = await cmd.getRegExp(message.channel);
+          // Test if the sub-command matches
+          const cmdMatch = regex.exec(group);
+
+          if (cmdMatch) {
+            // Execute the sub-command
+            await cmd.execute(message, cmdMatch);
+            return;
+          }
+        }
+
+        // No match found, execute default action
+        this.defaultAction(message, match);
+      },
+      role,
+    );
+
+    this.defaultAction = defaultAction;
+    this.commands = commands;
+  }
+}
